c921f611de76d7719ad76eee4982e5242cfde8c1
faizanafridi620 <faizanafridi676@gmail.com>
2026-06-07 16:34:52 +0530

    Update Cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const app = express();
 app.use(express.json())
 app.use(urlencoded({ extended: true}))
 app.use(cors({
-
+    origin: [
+        "http://localhost:5173",
+    ],
+    credentials: true
 }))
 
 import authRoutes from "./routes/authRoutes.js"
